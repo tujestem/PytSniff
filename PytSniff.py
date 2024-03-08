4a567926995559e90d1b1f1d3758d82b0e4c127d
tujestem <67292307+tujestem@users.noreply.github.com>
2024-03-08 22:22:02 +0100

    Update PytSniff.py

--- PytSniff.py
+++ PytSniff.py
@@ -1,32 +1,44 @@
+import keyboard
 from scapy.all import sniff, TCP, UDP, IP
 from termcolor import colored
+from threading import Thread
 
 def packet_callback(packet):
-    try:
-        if packet.haslayer(TCP) or packet.haslayer(UDP):
-            # get basic info about packets.
-            ip_src = packet[IP].src
-            ip_dst = packet[IP].dst
-            src_port = packet.sport
-            dst_port = packet.dport
-            frame_len = len(packet)
+    if packet.haslayer(TCP) or packet.haslayer(UDP):
 
-            # Start packets analysis.
-            errors = []
+        ip_src = packet[IP].src
+        ip_dst = packet[IP].dst
+        src_port = packet.sport
+        dst_port = packet.dport
+        frame_len = len(packet)
+        
+        if frame_len > 689:
+            print(colored(f"Error: packet from  {ip_src}:{src_port} do {ip_dst}:{dst_port} exceed example value of MTU (689 bytes) - {frame_len} bytes", "red"))
+        else:
+            print(f"packet from {ip_src}:{src_port} do {ip_dst}:{dst_port} - {frame_len} bytes")
 
-            # Check packet size.
-            if frame_len > 800:
-                errors.append("Size of the packet excess value of 800 (take it easy... normal MTU value is 1500)")
+should_sniff = True
 
-            # show packets info.
-            print(f"Pakiet {ip_src}:{src_port} -> {ip_dst}:{dst_port}, Rozmiar ramki: {frame_len}")
+def sniff_traffic():
+    global should_sniff
+    while should_sniff:
+        sniff(filter="tcp or udp", prn=packet_callback, store=0, timeout=1)
 
-            # errors 
-            for error in errors:
-                print(colored(error, "red"))
+def on_esc():
+    global should_sniff
+    should_sniff = False
+    print("Stopping...")
 
-    except Exception as e:
-        print(f"Wystąpił błąd podczas analizy pakietu: {e}")
+keyboard.on_press_key("esc", lambda _: on_esc())
 
-# listening mode.
-sniff(filter="tcp or udp", prn=packet_callback, store=0)
+sniff_thread = Thread(target=sniff_traffic)
+sniff_thread.start()
+
+try:
+    while should_sniff:
+        pass
+except KeyboardInterrupt:
+    should_sniff = False
+
+sniff_thread.join()
+print("program finished.")
