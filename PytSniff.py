ac61e817a05c37b5cc764457607f3917540a8a2a
tujestem <67292307+tujestem@users.noreply.github.com>
2024-03-08 21:45:11 +0100

    Update PytSniff.py

--- PytSniff.py
+++ PytSniff.py
@@ -4,7 +4,7 @@ from termcolor import colored
 def packet_callback(packet):
     try:
         if packet.haslayer(TCP) or packet.haslayer(UDP):
-            # Pobieranie podstawowych informacji o pakiecie
+            # get basic info about packets.
             ip_src = packet[IP].src
             ip_dst = packet[IP].dst
             src_port = packet.sport
@@ -18,15 +18,15 @@ def packet_callback(packet):
             if frame_len > 800:
                 errors.append("Size of the packet excess value of 800 (take it easy... normal MTU value is 1500)")
 
-            # Wyświetlanie informacji o pakiecie
+            # show packets info.
             print(f"Pakiet {ip_src}:{src_port} -> {ip_dst}:{dst_port}, Rozmiar ramki: {frame_len}")
 
-            # Jeśli są błędy, podkreśl je kolorem czerwonym
+            # errors 
             for error in errors:
                 print(colored(error, "red"))
 
     except Exception as e:
         print(f"Wystąpił błąd podczas analizy pakietu: {e}")
 
-# Ustaw nasłuchiwanie pakietów TCP i UDP
+# listening mode.
 sniff(filter="tcp or udp", prn=packet_callback, store=0)
